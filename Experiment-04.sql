d596ff9f55378e9fb89e525bb19c8937c0e71be3
Karan Singh <127474239+Karan-S1ngh@users.noreply.github.com>
2023-10-26 02:24:34 +0530

    DDL and DML Commands

--- Experiment-04.sql
+++ Experiment-04.sql
@@ -6,10 +6,10 @@ create table customer
     Gender varchar(9),  
     PhoneNo number(10),  
     mail varchar(18)  
-) 
+); 
 /*Table created. */
 
-desc customer
+desc customer;
 /*TABLE CUSTOMER
 Column	Null?	   Type
 ID	      - 	VARCHAR2(9)
@@ -20,10 +20,10 @@ PHONENO	  - 	NUMBER(10,0)
 MAIL	  - 	VARCHAR2(18)
 */
 
-alter table customer ADD address varchar(30) 
+alter table customer ADD address varchar(30); 
 /*Table altered. */
 
-desc customer 
+desc customer; 
 /*TABLE CUSTOMER
 Column	Null?	   Type
 ID	      - 	VARCHAR2(9)
@@ -35,69 +35,69 @@ MAIL	  - 	VARCHAR2(18)
 ADDRESS	  - 	VARCHAR2(30)
 */
 
-alter table customer MODIFY (address varchar(50)) 
+alter table customer MODIFY (address varchar(50)); 
 /*Table altered. */
 
-RENAME customer to Cusinfo 
+RENAME customer to Cusinfo; 
 /*Statement processed. */
 
-drop table Cusinfo 
+drop table Cusinfo; 
 /*Table dropped. */
 
-insert into customer Values('One','ran',0272006,'Male',12345,'tp@gmail.com') 
+insert into customer Values('One','ran',0272006,'Male',12345,'tp@gmail.com'); 
 /*1 row(s) inserted. */
 
-insert into customer Values('two','run',2262005,'Male',12045,'trfjp@gmail.com') 
+insert into customer Values('two','run',2262005,'Male',12045,'trfjp@gmail.com'); 
 /*1 row(s) inserted. */
 
-select * from customer 
+select * from customer; 
 /*
 ID	NAME	DOB	   GENDER	 PHONENO	MAIL
 One	ran	  272006	Male	 12345	  tp@gmail.com
 two	run	  2262005	Male	 12045	  trfjp@gmail.com
 */
 
-update customer set Name='karan' where ID='One'
+update customer set Name='karan' where ID='One'; 
 /*1 row(s) updated. */
 
-select * from customer 
+select * from customer; 
 /*
 ID	NAME	DOB	     GENDER	 PHONENO	MAIL
 One	karan  272006	 Male	   12345	tp@gmail.com
 two	run	  2262005	 Male	   12045    trfjp@gmail.com
 */
 
-select Name from customer
+select Name from customer; 
 /*
 NAME
 karan
 run
 */
 
-delete customer where ID='One'
+delete from customer where ID='One'; 
 /*1 row(s) deleted. */
 
-select distinct Phoneno from customer
+select distinct Phoneno from customer; 
 /*
 PHONENO
 12345
 12045
 */
 
-select * from customer where Phoneno IN 12345
+select * from customer where Phoneno IN (12345); 
 /*
 ID	NAME	DOB	    GENDER	PHONENO	 MAIL
 One	karan	272006	Male	  12345	   tp@gmail.com
 */
 
-select ID as something from customer
+select ID as something from customer; 
 /*
 SOMETHING
 One
 Two
 */
 
-select * from CUSTOMER where ID='One' OR ID='Two'
+select * from CUSTOMER where ID='One' OR ID='Two'; 
 /*
 ID	NAME	DOB	     GENDER	 PHONENO	MAIL
 One	karan  272006	 Male	   12345    tp@gmail.com
@@ -105,5 +105,5 @@ two	run	  2262005	 Male	   12045    trfjp@gmail.com
 2 rows selected
 */
 
-delete from customer
+delete from customer; 
 /*2 row(s) deleted. */
