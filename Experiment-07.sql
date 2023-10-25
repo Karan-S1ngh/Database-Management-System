a6e23bce9c556894c74cf02cbaf7a62dac9dfd3f
Karan Singh <127474239+Karan-S1ngh@users.noreply.github.com>
2023-10-26 02:16:08 +0530

    Simple queries and String manipulation operations.

--- Experiment-07.sql
+++ Experiment-07.sql
@@ -26,7 +26,7 @@ ID	NAME	  DOB	GENDER	PHONENO	    MAIL
 6	  Run	     - 	 - 	    1234567895	rn66@gmail.com
 7	  Saksham	 - 	 - 	    1234567896	sk77@gmail.com
 8	  Santosh	 - 	 -      1234567897	st88@gmail.com
-9	  Thomas   - 	 - 	    1234567898	ts99@gmail.com
+9	  Thomas     - 	 - 	    1234567898	ts99@gmail.com
 4 rows selected
 */
      
@@ -36,16 +36,16 @@ INSERT INTO CUS_DUPP1 SELECT * FROM CUSTOMER WHERE ID IN(SELECT ID FROM CUSTOMER
 /*9 row(s) inserted. */
 SELECT * FROM CUS_DUPP1 ORDER BY ID 
 /*
-ID	NAME	  DOB	GENDER	  PHONE	      MAIL
-1	  Ramesh	 - 	  - 	    1234567890	rm11@gmail.com
-2	  Rakesh	 - 	  - 	    1234567891	rk22@gmail.com
-3	  Sarvesh	 - 	  - 	    1234567892	srv33@gmail.com
-4	  Suresh	 - 	  - 	    1234567893	sur44@gmail.com
-5	  Karan	   - 	  - 	    1234567894	ks55@gmail.com
-6	  Run	     - 	  - 	    1234567895	rn66@gmail.com
-7	  Saksham	 - 	  - 	    1234567896	sk77@gmail.com
-8	  Santosh	 - 	  - 	    1234567897	st88@gmail.com
-9	  Thomas   - 	  - 	    1234567898	ts99@gmail.com
+ID	NAME	  DOB	GENDER	  PHONE	        MAIL
+1	  Ramesh	 - 	  - 	  1234567890	rm11@gmail.com
+2	  Rakesh	 - 	  - 	  1234567891	rk22@gmail.com
+3	  Sarvesh	 - 	  - 	  1234567892	srv33@gmail.com
+4	  Suresh	 - 	  - 	  1234567893	sur44@gmail.com
+5	  Karan	     - 	  - 	  1234567894	ks55@gmail.com
+6	  Run	     - 	  - 	  1234567895	rn66@gmail.com
+7	  Saksham	 - 	  - 	  1234567896	sk77@gmail.com
+8	  Santosh	 - 	  - 	  1234567897	st88@gmail.com
+9	  Thomas     - 	  - 	  1234567898	ts99@gmail.com
 9 rows selected.
 */
  
@@ -54,15 +54,15 @@ UPDATE CUS_DUPP1 SET ID=ID-1 WHERE ID IN (SELECT ID FROM CUSTOMER WHERE ID>=5)
 SELECT * FROM CUS_DUPP1 ORDER BY ID 
 /*
 ID	NAME	  DOB	GENDER	  PHONE	      MAIL
-1	  Ramesh	 - 	  - 	    1234567890	rm11@gmail.com
-2	  Rakesh	 - 	  - 	    1234567891	rk22@gmail.com
-3	  Sarvesh	 - 	  - 	    1234567892	srv33@gmail.com
-4	  Suresh	 - 	  - 	    1234567893	sur44@gmail.com
-4	  Karan	   - 	  - 	    1234567894	ks55@gmail.com
-5	  Run	     - 	  - 	    1234567895	rn66@gmail.com
-6	  Saksham	 - 	  - 	    1234567896	sk77@gmail.com
-7	  Santosh	 - 	  - 	    1234567897	st88@gmail.com
-8	  Thomas   - 	  - 	    1234567898	ts99@gmail.com
+1	  Ramesh	 - 	  - 	  1234567890	rm11@gmail.com
+2	  Rakesh	 - 	  - 	  1234567891	rk22@gmail.com
+3	  Sarvesh	 - 	  - 	  1234567892	srv33@gmail.com
+4	  Suresh	 - 	  - 	  1234567893	sur44@gmail.com
+4	  Karan	     - 	  - 	  1234567894	ks55@gmail.com
+5	  Run	     - 	  - 	  1234567895	rn66@gmail.com
+6	  Saksham	 - 	  - 	  1234567896	sk77@gmail.com
+7	  Santosh	 - 	  - 	  1234567897	st88@gmail.com
+8	  Thomas     - 	  - 	  1234567898	ts99@gmail.com
 9 rows selected.
 */
  
@@ -71,10 +71,10 @@ DELETE FROM CUS_DUPP1 WHERE ID IN (SELECT ID FROM CUSTOMER WHERE ID>=5)
 SELECT * FROM CUS_DUPP1 ORDER BY ID
 /*
 ID	NAME	  DOB	GENDER	  PHONE	      MAIL
-1	  Ramesh	 - 	  - 	    1234567890	rm11@gmail.com
-2	  Rakesh	 - 	  - 	    1234567891	rk22@gmail.com
-3	  Sarvesh	 - 	  - 	    1234567892	srv33@gmail.com
-4	  Suresh	 - 	  - 	    1234567893	sur44@gmail.com
-4	  Karan	   - 	  - 	    1234567894	ks55@gmail.com
+1	  Ramesh	 - 	  - 	  1234567890	rm11@gmail.com
+2	  Rakesh	 - 	  - 	  1234567891	rk22@gmail.com
+3	  Sarvesh	 - 	  - 	  1234567892	srv33@gmail.com
+4	  Suresh	 - 	  - 	  1234567893	sur44@gmail.com
+4	  Karan	     - 	  - 	  1234567894	ks55@gmail.com
 5 rows selected.
 */
